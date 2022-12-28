b5b1ba160e4a8a84ed45edb45b793d14e968d96f
takuyahara-apesborder <t.hara@apesborder.com>
2022-12-28 16:43:14 +0900

    add common.blade.php,header.blade.php

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+
+<header class='header'>
+    <div class='logo'>
+        ロゴ表示
+    </div>
+    <div class='user-info'>
+        USERNAME:<span>XXXXXXX</span>
+        TEAM:<span>XXXXXXXX</span>
+    <div>
+</header>
